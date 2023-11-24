5d73aa76b724fd12f66d3665dde88e7c2cfa226f
JOYWRLD <joywrld@proton.me>
2023-11-24 10:02:46 -0800

    Sidebar Update

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function myFunc(){
+    let a = document.getElementById('p')
+a.classList.toggle("active")
+}
+function login(){
+    let b = document.getElementById('text')
+    b.classList.toggle("hide")
+}
